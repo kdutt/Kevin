6be18357b60388e56fb0bb1fd48bb276866a0873
kdutt <kevin.dutton@gmail.com>
2014-02-27 16:45:37 -0500

    tset

--- admin/index.php
+++ admin/index.php
@@ -13,11 +13,13 @@
     Hi asdf
     <a href="pages.php">Page</a>
     <?php
-    printf("test");
-        $mypage = new page();
-        $mypage->get(1);
-        printf($mypage->id);
         printf("test");
+        $mypage = new page();
+        printf("test2");
+        $mypage->get(1);
+        printf("test3");
+        printf($mypage->id);
+        printf("test4");
     ?>
 </body>
 </html>
